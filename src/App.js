ae4ce947f5f6fbf502a83de009231bbb3f17071c
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2021-09-27 16:48:28 -0400

    created signup page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/signup' component={Signup} />
-        
       </Switch>
     </Router>
   );
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -6,7 +6,27 @@ function Signup() {
       <div className="columns is-mobile">
         <div className="column is-half is-offset-one-quarter">
           <div className="has-text-centered">
-            <h1 className="is-size-3">Welcome!</h1>
+            <h1 className="is-size-3">Please Signup</h1>
+          </div>
+          <div className="field">
+            <label className="label">First Name</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="First Name"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Last Name</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                placeholder="Last Name"
+              />
+            </div>
           </div>
           <div className="field">
             <label className="label">Email</label>
@@ -19,10 +39,17 @@ function Signup() {
             </div>
           </div>
 
+          
           <div className="field">
             <label className="label">Password</label>
             <div className="control">
               <input className="input" type="password" />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Confirm Password</label>
+            <div className="control">
+              <input className="input" type="text" />
             </div>
           </div>
           
@@ -33,3 +60,5 @@ function Signup() {
 }
 
 export default Signup;
+
+// Add in passwor confimration streghtn thing
