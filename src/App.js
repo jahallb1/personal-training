91b29e0d7808bba8aa30c256c49e59dcacb41c56
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2021-09-27 12:49:32 -0400

    added ned changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router,  Route, Switch } from 'react-router-dom';
+
 import Login from "./pages/Login";
+import Signup from "./pages/Signup"
+import Home from "./pages/Home"
 
 
 function App() {
   return (
-    <div className="App">
-     <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route excat path='/' component={Home} />
+        <Route excat path='/login' component={Login} />
+        <Route excat path='/singup' component={Signup} />
+        
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            Here
+        </div>
+    )
+}
+
+export default Home
